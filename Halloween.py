51919db5be41331139fa8d4e013b0ba8631bb79c
47_ <94054622+csq95@users.noreply.github.com>
2022-11-06 22:18:18 -0500

    Update Halloween.py

--- Halloween.py
+++ Halloween.py
@@ -1,11 +1,17 @@
 import streamlit as st
-
-st.title("Happy Halloween!")
-
-st.write(
-    "Celebrate this Halloween by playing with the pages to the left, editing the code, and then publishing your own cool Streamlit demo online."
-)
+import numpy as np
+st.title("Happy Weekend!")
 
 st.write(
-    "Feel free to consult [the instructions](https://msu-ai.notion.site/Deploying-AI-Projects-with-Streamlit-b10903703adf45b6b546fc3c050cb893) if you ever get stuck, or ask a mentor or the people around you."
+    "Let's try a to find the largest number!"
 )
+num1 = st.number_input("Enter first number = ")
+num2 = st.number_input("Enter second number = ")
+num3 = st.number_input("Enter third number = ")
+
+if (num1>=num2) and (num1>=num3) :
+    st.write(num1,"is the largest.")
+elif (num2>=num1) and (num2>=num3) :
+    st.write(num2,"is the largest.")
+else :
+    st.write(num3,"is the largest.")
